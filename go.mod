c841d1ebba9a97d9a88b3f2a22825bc45178206c
Sam Lavigne <splavigne@gmail.com>
2021-03-04 10:53:25 -0500

    run at load

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module zoom.lav.io/zoom_deleter/v2
 go 1.15
 
 require (
+	github.com/emersion/go-autostart v0.0.0-20210130080809-00ed301c8e9a
 	github.com/getlantern/systray v1.1.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	// "github.com/emersion/go-autostart"
+	"github.com/emersion/go-autostart"
 	"github.com/getlantern/systray"
 	"github.com/mitchellh/go-homedir"
 	"os"
@@ -12,6 +12,8 @@ import (
 	"zoom.lav.io/zoom_deleter/v2/icon"
 )
 
+//os.Executable()
+
 func main() {
 	onExit := func() {
 	}
@@ -20,36 +22,71 @@ func main() {
 }
 
 func onReady() {
+	executable, _ := os.Executable()
+	app := &autostart.App{
+		Name:        "zoomdeleter",
+		DisplayName: "Zoom Deleter",
+		Exec:        []string{executable},
+	}
+
 	systray.SetIcon(icon.Data)
 	// systray.SetTitle("Zoom Deleter")
 	systray.SetTooltip("Zoom Deleter")
 	mInfo := systray.AddMenuItem("Status: Active", "Status: Active")
 	mInfo.Disable()
 
+	runningOnStart := app.IsEnabled()
+	runningMessage := "Run At Start"
+	mStart := systray.AddMenuItemCheckbox(runningMessage, runningMessage, runningOnStart)
+
 	systray.AddSeparator()
 
 	mQuitOrig := systray.AddMenuItem("Quit", "Quit Zoom Deleter")
-	go func() {
-		<-mQuitOrig.ClickedCh
-		fmt.Println("Requesting quit")
-		systray.Quit()
-		fmt.Println("Finished quitting")
-	}()
 
 	deleteTicker := time.NewTicker(5 * time.Second)
 	done := make(chan bool)
 
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case t := <-deleteTicker.C:
-				fmt.Println("Tick at", t)
-				deleter()
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-deleteTicker.C:
+			fmt.Println("Tick at", t)
+			deleter()
+		case <-mStart.ClickedCh:
+			if mStart.Checked() {
+				mStart.Uncheck()
+				fmt.Println("Unchecking")
+				if err := app.Disable(); err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				mStart.Check()
+				fmt.Println("Checking")
+				if err := app.Enable(); err != nil {
+					fmt.Println(err)
+				}
 			}
+		case <-mQuitOrig.ClickedCh:
+			systray.Quit()
+			return
 		}
-	}()
+	}
+
+	// deleteTicker := time.NewTicker(5 * time.Second)
+	// done := make(chan bool)
+	//
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case <-done:
+	// 			return
+	// 		case t := <-deleteTicker.C:
+	// 			fmt.Println("Tick at", t)
+	// 			deleter()
+	// 		}
+	// 	}
+	// }()
 }
 
 func deleter() {
